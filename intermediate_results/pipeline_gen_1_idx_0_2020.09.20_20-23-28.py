6320862f0ddf24cf7a928d89a27bfeac407cafc5
Mandar Karhade <man.karhade@gmail.com>
2020-09-20 22:22:31 -0500

    Created TPOT working version
    I needed a cleaner start for TPOT
    1. Fixed Y encoding
    2. Added MultiopleColumnEncoding
    3. Streamlined original and encoded variables
    4. Continued with added information on the metrics of prediction

--- intermediate_results/pipeline_gen_1_idx_0_2020.09.20_20-23-28.py
+++ intermediate_results/pipeline_gen_1_idx_0_2020.09.20_20-23-28.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -10,7 +10,7 @@ training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=1)
 
 # Average CV score on the training set was: 1.0
-exported_pipeline = DecisionTreeRegressor(max_depth=8, min_samples_leaf=3, min_samples_split=20)
+exported_pipeline = KNeighborsRegressor(n_neighbors=1, p=1, weights="distance")
 # Fix random state in exported estimator
 if hasattr(exported_pipeline, 'random_state'):
     setattr(exported_pipeline, 'random_state', 1)
